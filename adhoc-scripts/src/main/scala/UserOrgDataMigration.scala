a57bfd413b4aeb3bb2959d0a8eb47d906754e9d3
AMIT KUMAR <amit.kumar@tarento.com>
2020-08-31 17:08:41 +0530

    Issue #TG-462 feat:merge roles of duplicates

--- adhoc-scripts/src/main/scala/UserOrgDataMigration.scala
+++ adhoc-scripts/src/main/scala/UserOrgDataMigration.scala
@@ -3,6 +3,8 @@ import org.apache.spark.sql.{ SparkSession }
 import org.apache.spark.sql.Encoders
 import org.apache.spark.sql.SaveMode
 import org.apache.spark.storage.StorageLevel
+import org.apache.spark.sql.functions.{array_distinct, flatten}
+import org.apache.spark.sql.expressions.Window
 case class UserOrganisation(userid: String, organisationid: String, addedby: Option[String], addedbyname: Option[String], approvaldate: Option[String],
                             approvedby: Option[String], hashtagid: Option[String], id: String, isapproved: Option[Boolean], isdeleted: Option[Boolean],
                             isrejected: Option[Boolean], orgjoindate: Option[String], orgleftdate: Option[String], position: Option[String],
@@ -24,11 +26,18 @@ object UserOrgDataMigration extends Serializable {
   }
   def migrateData()(implicit spark: SparkSession) {
     val schema = Encoders.product[UserOrganisation].schema
-    val data = spark.read.format("org.apache.spark.sql.cassandra").schema(schema).option("keyspace", "sunbird").option("table", "user_org").load().persist(StorageLevel.MEMORY_ONLY)
-    println("user_org data Count : " + data.count())
-    val filteredData = data.where(col("userid").isNotNull && col("organisationid").isNotNull)
-    filteredData.write.format("org.apache.spark.sql.cassandra").option("keyspace", "sunbird").option("table", "user_organisation").mode(SaveMode.Append).save()
-    println("user_organistaion count post migration: " + filteredData.count())
+    val data = spark.read.format("org.apache.spark.sql.cassandra").schema(schema).option("keyspace", "sunbird").option("table", "user_org").load();
+    val filteredData = data.where(col("userid").isNotNull && col("organisationid").isNotNull).persist(StorageLevel.MEMORY_ONLY)
+    println("user_org data Count : " + filteredData.count());
+    val distinctRecords = filteredData.dropDuplicates("userid", "organisationid").persist();
+    val dupRecords = filteredData.except(distinctRecords).select(col("userid"),col("organisationid"),col("roles")).persist(StorageLevel.MEMORY_ONLY);
+    val distinctRecordsWithRoles = distinctRecords.select(col("userid"),col("organisationid"),col("roles"));
+    val distinctRoleDF = distinctRecordsWithRoles.join(dupRecords, distinctRecordsWithRoles("userid") === dupRecords("userid") && distinctRecordsWithRoles("organisationid") === dupRecords("organisationid"), "leftsemi");
+    val rolesDF = dupRecords.union(distinctRoleDF).withColumn("role", explode(col("roles"))).groupBy("userid","organisationid").agg(collect_set("role").as("roles"));
+    distinctRecords.write.format("org.apache.spark.sql.cassandra").option("keyspace", "sunbird").option("table", "user_organisation").mode(SaveMode.Append).save()
+    rolesDF.write.format("org.apache.spark.sql.cassandra").option("keyspace", "sunbird").option("table", "user_organisation").mode(SaveMode.Append).save()
+    val newTableRecords = spark.read.format("org.apache.spark.sql.cassandra").schema(schema).option("keyspace", "sunbird").option("table", "user_organisation").load().count();
+    println("user_organistaion count post migration: " + newTableRecords)
   }
   def time[R](block: => R): (Long, R) = {
     val t0 = System.currentTimeMillis()
